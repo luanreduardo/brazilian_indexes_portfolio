1e84cae1a2b29c8e45bfae46605e5b9fd5689e45
luanreduardo <luan.eduardo@ufrgs.br>
2019-10-27 19:38:43 -0300

    applying br indexes

--- codes/risk_optimal_portfolio/01_minCVaR_min_Var_backtest.R
+++ codes/risk_optimal_portfolio/01_minCVaR_min_Var_backtest.R
@@ -6,9 +6,11 @@ library(FRAPO) #for the data set
 library(fPortfolio) #for the routines
 
 #retriving data and calculating returns
-data("StockIndex")
-StockReturn <- na.omit(timeSeries(returnseries(StockIndex, method = 'discrete'),
-                                  charvec = rownames(StockIndex))) #computing discrete % returns
+br_indexes <- read.csv('data/pricesIndBr.csv')
+rownames(br_indexes) <- br_indexes[, 'Index']
+br_indexes <- br_indexes[, -1]
+br_returns <- na.omit(timeSeries(returnseries(br_indexes, method = 'discrete'),
+                                  charvec = rownames(br_indexes))) #computing discrete % returns
 
 #specifying portfolio
 pspec <- fPortfolio::portfolioSpec() #default specification
@@ -20,14 +22,14 @@ fPortfolio::setType(cvar) <- 'CVaR' #type adjustment of the CVaR portfolio
 fPortfolio::setAlpha(cvar) <- .10 #confidence level
 fPortfolio::setSolver(cvar) <- "solveRglpk.CVAR" #solver adjustment of the CVaR port
 
-#conducting back-test, subsamples of 60 monthly observations (5yrs)
-end <- time(StockReturn)[60:239] #end point for sliding window
-from <- time(StockReturn)[1:length(end)] #start point for sliding window
-wGMV <- matrix(NA, ncol = ncol(StockReturn), nrow = length(end)) #defining matrices, weights as rows
+#conducting back-test
+end <- time(br_returns)[1260:5875] #end point for sliding window
+from <- time(br_returns)[1:length(end)] #start point for sliding window
+wGMV <- matrix(NA, ncol = ncol(br_returns), nrow = length(end)) #defining matrices, weights as rows
 wCVAR <- wGMV
 
 for (i in 1:length(end)) {
-  series <- window(StockReturn, start = from[i], end = end[i]) #relevant data window
+  series <- window(br_returns, start = from[i], end = end[i]) #relevant data window
   gmvpf <- fPortfolio::minvariancePortfolio(data = series, spec = gmv,
                                 constraints = 'LongOnly')
   wGMV[i, ] <- c(getWeights(gmvpf))
