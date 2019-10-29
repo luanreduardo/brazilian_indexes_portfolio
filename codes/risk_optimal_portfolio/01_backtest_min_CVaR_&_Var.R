dc901da7c40d111450ebe456ded21da2d4c425db
luanreduardo <luan.eduardo@ufrgs.br>
2019-10-28 22:43:27 -0300

    all set

---
+++ codes/risk_optimal_portfolio/01_backtest_min_CVaR_&_Var.R
@@ -0,0 +1,96 @@
+#### Based on cap 12 of Financial Risk Modelling
+####and Portfolio Optimization by Berhnard Pfaff
+
+#code 01
+
+##minimum-CVaR vs minimum-variance portfolios: back-test
+
+library(FRAPO) #for the data set
+library(fPortfolio) #for the routines
+
+#retriving data and calculating returns
+br_indexes <- read.csv('data/pricesIndBr.csv')
+rownames(br_indexes) <- br_indexes[, 'Index']
+br_indexes <- br_indexes[, -1]
+br_returns <- na.omit(returnseries(br_indexes, method = 'discrete'),
+                                 charvec = rownames(br_indexes)) #computing discrete % returns
+
+#defining timeseries
+ts_br_indexes <- timeSeries(br_indexes)
+ts_br_returns <- timeSeries(br_returns)
+
+#specifying portfolio
+pspec <- fPortfolio::portfolioSpec() #default specification
+
+gmv <- pspec #defining GMV as a portfolio with default specifications
+
+cvar <- pspec #defining a port with default for later ajustments for CVaR
+fPortfolio::setType(cvar) <- 'CVaR' #type adjustment of the CVaR portfolio
+fPortfolio::setAlpha(cvar) <- .10 #confidence level
+fPortfolio::setSolver(cvar) <- "solveRglpk.CVAR" #solver adjustment of the CVaR port
+
+#conducting back-test
+end <- time(ts_br_returns)[1260:5875] #end point for sliding window
+from <- time(ts_br_returns)[1:length(end)] #start point for sliding window
+wGMV <- matrix(NA, ncol = ncol(ts_br_returns), nrow = length(end)) #defining matrices, weights as rows
+wCVAR <- wGMV
+
+#TAKES TIME TO COMPUTE
+for (i in 1:length(end)) {
+  series <- window(ts_br_returns, start = from[i], end = end[i]) #relevant data window
+  gmvpf <- fPortfolio::minvariancePortfolio(data = series, spec = gmv,
+                                            constraints = 'LongOnly')
+  wGMV[i, ] <- c(getWeights(gmvpf))
+  cvarpf <- fPortfolio::minriskPortfolio(data = series, spec = cvar,
+                                         constraints = 'LongOnly')
+  wCVAR[i, ] <- c(getWeights(cvarpf))
+  print(i)
+}
+
+#saving weights
+saveRDS(wGMV, file = 'data/GMV_weights.RDS')
+saveRDS(wCVAR, file = 'data/CDaR_weights.RDS')
+
+####plotting wealth trajectory
+
+#lagged portfolio weights for equity lines computing
+wGMVL1 <- lag(timeSeries(wGMV, charvec = end), k = 1)
+colnames(wGMVL1) <- colnames(ts_br_returns)
+wCVARL1 <- lag(timeSeries(wCVAR, charvec = end), k = 1)
+colnames(wCVARL1) <- colnames(ts_br_returns)
+
+#computing od returns according to the pre-definied weights, lagged a period
+GMVRetFac <- 1 + rowSums(wGMVL1 * ts_br_returns[time(wGMVL1), ]) / 100 #return factor
+GMVRetFac[1] <- 100
+GMVPort <- timeSeries(cumprod(GMVRetFac), 
+                      charvec = names(GMVRetFac)) #portfolio values
+
+CVARRetFac <- 1 + rowSums(wCVARL1 * ts_br_returns[time(wCVARL1), ]) / 100 #return factor
+CVARRetFac[1] <- 100
+CVARPort <- timeSeries(cumprod(CVARRetFac),
+                       charvec = names(CVARRetFac)) #portfolio values
+
+saveRDS(GMVPort, file = 'data/GMV_port_values.RDS')
+saveRDS(wCVAR, file = 'data/CDaR_port_values.RDS')
+
+#plotting of portfolio values
+ylims <- range(cbind(GMVPort, CVARPort))
+plot(GMVPort, ylim = ylims, xlab = '',
+     ylab = 'Portfolio Value (Index)')
+lines(CVARPort, col = 'blue')
+legend('topleft',
+       legend = c('Minimum-Variance', 'Minimum-CVaR'),
+       col = c('black', 'blue'), lty = 1)
+
+dev.copy(png, 'results/port_value.png') #saving the graph
+dev.off()
+
+#relative performance
+RelOutPerf <- (CVARPort - GMVPort) / GMVPort * 100
+plot(RelOutPerf, type = 'h', col = 'blue', xlab = '',
+     ylab = 'Percentage',
+     main = 'Relative out-performance Min-CVaR vs. Min-Variance')
+abline(h = 0, col = 'grey')
+
+dev.copy(png, 'results/relative_performance.png')
+dev.off()
---
+++ codes/risk_optimal_portfolio/02_drawdowns_vs_GMV.R
@@ -0,0 +1,94 @@
+#code 02
+
+####comparison draw-down and GMV portfolio
+
+library(fPortfolio) #for construction resources
+library(FRAPO) #for data set
+library(PerformanceAnalytics) #for analysis resources
+
+
+#time series of discrete returns
+Rets <- returnseries(br_indexes, method = 'discrete',
+                     percentage = F, trim = T) #in ts_br_returns, trim = F
+Rets <- timeSeries(Rets, charvec = rownames(Rets))
+
+#benchmark portfolio - GMV
+gmvspec <- portfolioSpec()
+GMV <- minvariancePortfolio(data = Rets, spec = gmvspec,
+                            constraints = 'LongOnly')
+GMVret <- timeSeries(Rets %*% getWeights(GMV), #time series of portfolio returns
+                     charvec = time(Rets))
+GMVDD <- PerformanceAnalytics::Drawdowns(GMVret) #historical draw-drowns
+
+#plot of draw-downs for GMV
+ylims <- c(-25, 0)
+plot(GMVDD * 100, xlab = '', ylab = 'Draw-downs (percentage)',
+     main = 'Draw-downs Global Minimum-Variance',
+     ylim = ylims)
+abline(h = 0, col = 'grey')
+grid()
+
+dev.copy(png, 'results/drawdowns_GMV.png')
+dev.off()
+
+#max draw-down of GMV
+GMVMaxDD <- max(-1.0 * GMVDD)
+
+#computing CDaR 95% draw-downs from GMV draw-downs TAKES TIME
+MaxDD <- PMaxDD(br_indexes, MaxDD = GMVMaxDD)
+AveDD <- PAveDD(br_indexes, AveDD = GMVMaxDD)
+CDaR95 <- PCDaR(br_indexes, alpha = .95, bound = GMVMaxDD)
+CDaRMin95 <- PCDaR(br_indexes, alpha = .95)
+
+#plot of draw-downs
+oldpar <- par(no.readonly = T)
+par(mfrow = c(2, 2))
+
+plot(AveDD, main = '(a) AveDD')
+plot(MaxDD, ylim = ylims, main = '(b) MaxDD')
+plot(CDaR95, ylim = ylims, main = '(c) CDaR')
+plot(CDaRMin95, ylim = ylims, main = '(d) Minimum CDaR')
+
+dev.copy(png, 'results/drawdowns_&_CDaR.png')
+dev.off()
+
+par(oldpar)
+
+####analysis of portfolio solutions
+
+#names for the 5 portfolio types
+Pnames <- c('GMV', 'MaxDD', 'AveDD', 'CDaR95', 'CDaRMin95')
+
+#creating a matrix with all portfolio allocations
+WeightMatrix <- cbind(getWeights(GMV),
+                      Weights(MaxDD),
+                      Weights(AveDD),
+                      Weights(CDaR95),
+                      Weights(CDaRMin95))
+colnames(WeightMatrix) <- Pnames
+
+#expected shortfall and components
+tmp <- apply(WeightMatrix, 2, function(x) ES(Rets, weights = x,
+                                             method = 'gaussian',
+                                             portfolio_method = 'component'))
+
+#ES 95%
+PES <- unlist(lapply(tmp, function(x) x[[1]])) * 100
+
+#marginal contributions to ES
+PMES <- matrix(unlist(lapply(tmp, function(x) x[[3]])),
+               nrow = ncol(Rets)) * 100
+rownames(PMES) <- colnames(Rets)
+colnames(PMES) <- Pnames
+
+#marginal contributions to standard deviation
+V <- cov(Rets)
+PMRC <- apply(WeightMatrix, 2, mrc, Sigma = V)
+rownames(PMRC) <- colnames(Rets)
+
+#diversification ratio
+PDR <- apply(WeightMatrix, 2, dr, Sigma = V)
+
+### SET A SINGLE TABLE AND SAVE
+portfolio_analysis <- 
+saveRDS(portfolio_analysis, file = 'results/portfolio_analysis.RDS')
