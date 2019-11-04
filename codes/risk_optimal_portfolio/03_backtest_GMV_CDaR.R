a9fb40daed07cc92366b16d46cdfa86e67324550
Rezende Eduardo <luan.eduardo@ufrgs.br>
2019-11-03 23:35:52 -0300

    adjusted

--- codes/risk_optimal_portfolio/03_backtest_GMV_CDaR.R
+++ codes/risk_optimal_portfolio/03_backtest_GMV_CDaR.R
@@ -35,18 +35,18 @@ wMV <- wCD <- matrix(NA, ncol = ncol(RDP), nrow = length(to))
 #coducting backtest (ENJOY YOUR LIFE, IT'S GONNA TAKE FOREVER)
 for (i in 1:length(to)) {
   series <- window(RDP, start = from[i], end = to[i])
-  #prices <- window(pr, start = from[i], end = to[i])
+  prices <- window(pr, start = from[i], end = to[i])
   mv <- minvariancePortfolio(data = series,
                              spec = mvspec,
                              constraints = BoxC)
-  #cd <- PCDaR(prices, alpha = DDalpha, bound = DDbound, softBudget = T)
+  cd <- PCDaR(prices, alpha = DDalpha, bound = DDbound, softBudget = T)
   wMV[i, ] <- c(getWeights(mv))
-  #wCD[i, ] <- Weights(cd)
+  wCD[i, ] <- Weights(cd)
   print(i)
 }
 
 saveRDS(wMV, 'data/MV_weights.Rds')
-#saveRDS(wCD, 'data/CDaR_weights.Rds')
+saveRDS(wCD, 'data/CDaR_weights.Rds')
 
 #lagging optimal weights and sub-sample of returns
 wMV <- rbind(rep(NA, ncol(RDP)), wMV[-nrow(wMV), ])
@@ -60,7 +60,7 @@ colnames(wCDL1) <- colnames(RDP)
 RDPback <- RDP[to, ]
 colnames(RDPback) <- colnames(RDP)
 
-####backtest evaluation, part 01
+####backtest evaluation
 
 #portfolio equities of strategies
 MVRetFac <- 1 + rowSums(wMVL1 * RDPback) / 100
@@ -102,44 +102,28 @@ abline(h = -10, col = 'lightgrey', lty = 2)
 legend('bottomleft', legend = c('CDaR', 'GMV'),
        col = c('black', 'blue'), lty = 1)
 
-####backtest evaluation, part 01
+#VaR
+MVVAR <- -100 * VaR(MVRet, p = .95, method = 'gaussian')
+CDVAR <- -100 * VaR(CDRet, p = .95, merhod = 'gaussian')
 
-#portfolio equities of strategies
-MVRetFac <- 1 + rowSums(wMVL1 * RDPback) / 100
-MVRetFac[1] <- 100
-MVPort <- timeSeries(cumprod(MVRetFac),
-                     charvec = names(MVRetFac))
+#ES
+MVES <- -100 * ES(MVRet, p = .95, method = 'gaussian')
+CDES <- -100 * ES(CDRet, p = .95, method = 'gaussian')
 
-CDRetFac <- 1 + rowSums(wCDL1 * RDPback) / 100
-CDRetFac[1] <- 100
-CDPort <- timeSeries(cumprod(CDRetFac),
-                     charvec = names(CDRetFac))
+#sharpe
+MVSR <- SharpeRatio(MVRet)
+CDSR <- SharpeRatio(CDRet)
 
-#progression of wealth
-ylims <- range(cbind(MVPort, CDPort))
-plot(CDPort, main = '', ylim = ylims, ylab = 'Index values', xlab = '')
-lines(MVPort, col = 'darkgrey')
-legend('topleft', legend = c('CDaR', 'GMV'),
-       col = c('black', 'darkgrey'),
-       lty = 1)
+#annualised returns
+MVRA <- Return.annualized(MVRet, scale = 52)
+CDRA <- Return.annualized(CDRet, scale = 52)
 
-#portfolio returns
-MVRet <- returns(MVPort, method = 'discrete',
-                 percentage = F, trim = T)
-CDRet <- returns(CDPort, method = 'discrete',
-                 percentage = F, trim = T)
-
-#draw-down table
-table.Drawdowns(MVRet)
-table.Drawdowns(CDRet)
-
-#plor of draw-down curves
-MVD <- 100 * PerformanceAnalytics:::Drawdowns(MVRet)
-CDD <- 100 * PerformanceAnalytics:::Drawdowns(CDRet)
-plot(CDD, main = '', ylab = 'Percentages', xlab = '',
-     ylim = c(min(c(MVD, CDD)), 0))
-lines(MVD, col = 'blue')
-abline(h = 0, col = 'lightgrey')
-abline(h = -10, col = 'lightgrey', lty = 2)
-legend('bottomleft', legend = c('CDaR', 'GMV'),
-       col = c('black', 'blue'), lty = 1)
+#draw-downs
+MVDD <- -100 * findDrawdowns(MVRet)$return
+MVDD <- MVDD[MVDD != 0.0]
+length(MVDD)
+summary(MVDD)
+CDDD <- -100 * findDrawdowns(CDRet)$return
+CDDD <- CDDD[CDDD != 0.0]
+length(CDDD)
+summary(CDDD)
